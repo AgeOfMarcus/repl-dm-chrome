f2c2db6e69e852e9a59e3b45b98ce4e6d0baf4a2
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 15:07:00 +0000

    im gonna commit sewrslide

--- foreground.js
+++ foreground.js
@@ -1,5 +1,8 @@
 console.log('foreground baby');
 
-$('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
-    console.log('hell fuckin yeah');
-})
+$('body').on('DOMSubtreeModified', 'mydiv', function(){
+    console.log('changed');
+    $('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
+        console.log('hell fuckin yeah');
+    })
+});
