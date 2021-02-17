80e951eafac4bdf042f9e93c3aeaffc2786f1d40
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 15:21:25 +0000

    pls

--- foreground.js
+++ foreground.js
@@ -3,6 +3,6 @@ console.log('foreground baby');
 $('html').on('DOMSubtreeModified', 'body', function(){
     $('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
         console.log('hell fuckin yeah');
-        console.log($(this).parents('.jsx-1369737386:not(.user-info-card-header)').find('.user-label').attr('href'));
+        console.log($('.user-hover-card-anchor span.jsx-801033477').text());
     })
 });
