50caafc9707668e3eecd997ec8919d8927b11372
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 15:08:15 +0000

    asfafawg

--- foreground.js
+++ foreground.js
@@ -1,6 +1,6 @@
 console.log('foreground baby');
 
-$('body').on('DOMSubtreeModified', 'mydiv', function(){
+$('html').on('DOMSubtreeModified', 'body', function(){
     console.log('changed');
     $('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
         console.log('hell fuckin yeah');
