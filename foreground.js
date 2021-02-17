15e27cc9cccab06b647b8bc3e08f19186703b293
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 19:01:38 +0000

    hmm

--- foreground.js
+++ foreground.js
@@ -1,6 +1,6 @@
 console.log('foreground baby');
 
-$('html').on('DOMSubtreeModified', 'body', () =>) {
+$('html').on('DOMSubtreeModified', 'body', () => {
     $('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
         var username = $('.user-hover-card-anchor span.jsx-801033477').text().split(" ")[0];
         console.log(username);
