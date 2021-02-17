2fb23ce5e7837389503a79e77800fd21868ef1c6
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 14:54:21 +0000

    i hate css

--- foreground.js
+++ foreground.js
@@ -1,15 +1,5 @@
 console.log('foreground baby');
 
-$('.user-label').hover((event) => {
-    var href = $(event.target).attr('href');
-    console.log(href)
-    href = event.target.getAttribute('title')
-    setTimeout(() => {
-        console.log($(`.user-label[title='${href}']`))
-        var ele = $(`.user-label[title='${href}'] + .user-hover-card-anchor`).find('.user-info-card-header');
-        var html = ele.html();
-        if (!html.includes('</button>')) { // doesnt place multiple buttons
-            ele.html(html + '<button class="dmBtn">Message</button>');
-        };
-    }, 100);
+$('.user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
+    console.log('hell fuckin yeah');
 })
