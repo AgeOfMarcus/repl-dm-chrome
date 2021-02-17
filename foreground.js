d7cc276dfce75017d844e1dbd3c5d8dc9e2ec6d9
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 15:41:19 +0000

    goddamn tis is cool

--- foreground.js
+++ foreground.js
@@ -2,7 +2,15 @@ console.log('foreground baby');
 
 $('html').on('DOMSubtreeModified', 'body', function(){
     $('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
-        console.log('hell fuckin yeah');
-        console.log($('.user-hover-card-anchor span.jsx-801033477').text());
+        var username = $('.user-hover-card-anchor span.jsx-801033477')[0].innerText;
+        console.log(username);
     })
 });
+
+
+var html = `
+<div>
+    <i class="fas fa-paper-plane"></i>
+    <i class="far fa-paper-plane"></i>
+</div>`;
+$('header .right').before(html);
