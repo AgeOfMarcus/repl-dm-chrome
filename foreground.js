7d0e48fa5d28c69192a5c0ecde53e05277f3af20
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 14:30:22 +0000

    i push

--- foreground.js
+++ foreground.js
@@ -3,10 +3,10 @@ console.log('foreground baby');
 $('.user-label').hover((event) => {
     var href = $(event.target).attr('href');
     console.log(href)
-    console.log(event.target.getAttribute('href'))
+    href = event.target.getAttribute('title')
     setTimeout(() => {
-        console.log($(`.user-label[href='${href}']`))
-        var ele = $(`.user-label[href='${href}'] + .user-hover-card-anchor`).find('.user-info-card-header');
+        console.log($(`.user-label[title='${href}']`))
+        var ele = $(`.user-label[title='${href}'] + .user-hover-card-anchor`).find('.user-info-card-header');
         var html = ele.html();
         if (!html.includes('</button>')) { // doesnt place multiple buttons
             ele.html(html + '<button class="dmBtn">Message</button>');
