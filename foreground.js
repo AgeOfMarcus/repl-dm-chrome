060454f2f7bcba26c1b581fb519b98cad4738061
Marcus Weinberger <marcusjw.ftw@gmail.com>
2021-02-17 15:14:21 +0000

    lmao

--- foreground.js
+++ foreground.js
@@ -1,9 +1,8 @@
 console.log('foreground baby');
 
 $('html').on('DOMSubtreeModified', 'body', function(){
-    console.log('changed');
     $('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
         console.log('hell fuckin yeah');
-        console.log($(this).parent().find('.user-label').attr('href'));
+        console.log($(this).closest().find('.user-label').attr('href'));
     })
 });
