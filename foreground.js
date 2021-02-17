3082bb9f544204d638c1cfa3c48bd603c574c98e
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 19:34:25 +0000

    s

--- foreground.js
+++ foreground.js
@@ -3,9 +3,13 @@ console.log('foreground baby');
 var path = window.location.pathname;
 $('html').on('DOMSubtreeModified', 'body', () => {
     if (window.location.pathname !== path) {
-        console.log('path changed')
-        setup();
-        path = window.location.pathname;
+        setTimeout(() => {
+            window.addEventListener('load', (event) => {
+                console.log('path changed')
+                setup();
+                path = window.location.pathname;
+            });
+        }, 100)
     }
     $('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
         var username = $('.user-hover-card-anchor span.jsx-801033477').text().split(" ")[0];
