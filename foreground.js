a798184694a052dd1624d3c0c1319bc393e7fa50
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 14:00:13 +0000

    gaeafw

--- foreground.js
+++ foreground.js
@@ -2,7 +2,9 @@
 
 $('.user-label').hover(() => {
     var href = $(this).attr('href');
+    console.log(href)
     setTimeout(() => {
+        console.log($(`.user-label[href='${href}']`)
         var ele = $(`.user-label[href='${href}'] + .user-hover-card-anchor`).find('.user-info-card-header');
         var html = ele.html();
         if (!html.includes('</button>')) { // doesnt place multiple buttons
