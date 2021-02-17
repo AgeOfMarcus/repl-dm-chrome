675ae7fcd39b8f606adfa98666a2773f15da6369
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 13:58:48 +0000

    i hatert my lief

--- foreground.js
+++ foreground.js
@@ -2,9 +2,11 @@
 
 $('.user-label').hover(() => {
     var href = $(this).attr('href');
-    var ele = $(`.user-label[href='${href}'] + .user-hover-card-anchor`).find('.user-info-card-header');
-    var html = ele.html();
-    if (!html.includes('</button>')) { // doesnt place multiple buttons
-        ele.html(html + '<button class="dmBtn">Message</button>');
-    };
+    setTimeout(() => {
+        var ele = $(`.user-label[href='${href}'] + .user-hover-card-anchor`).find('.user-info-card-header');
+        var html = ele.html();
+        if (!html.includes('</button>')) { // doesnt place multiple buttons
+            ele.html(html + '<button class="dmBtn">Message</button>');
+        };
+    }, 100);
 })
