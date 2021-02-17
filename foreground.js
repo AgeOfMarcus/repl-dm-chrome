e4f3dd58eaa989c23bf6d3b84db615db71389f31
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 13:42:36 +0000

    poop

--- foreground.js
+++ foreground.js
@@ -1,16 +1,10 @@
 
-// adds message button to users 
-var el = document.getElementsByClassName("user-hover-card");
-for (i=0; i<el.length; i++){
-    el[i].addEventListener("mouseover", (event) => {
-        setTimeout(()=>{
-            var ele = event.target.querySelector('.user-info-card-header');
-            var html = ele.innerHTML;
-            if (!html.includes('</button>')) { // doesnt place multiple buttons
-                ele.innerHTML = html + '<button class="dmBtn">Message</button>';
-            };
-        }, 100);
-    });
-}
 
-console.log($('body'))
+$('.user-label').hover(() => 
+    var href = $(this).attr('href');
+    var ele = $(`.user-label[href='${href}'] + .user-hover-card-anchor`).querySelector('.user-info-card-header');
+    var html = ele.innerHTML;
+    if (!html.includes('</button>')) { // doesnt place multiple buttons
+        ele.innerHTML = html + '<button class="dmBtn">Message</button>';
+    };
+})
