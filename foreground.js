fd495c6fdf7c4a8acd2299d0b4c9bfd842293f6b
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 13:55:09 +0000

    3rd teiem is charming

--- foreground.js
+++ foreground.js
@@ -2,7 +2,7 @@
 
 $('.user-label').hover(() => {
     var href = $(this).attr('href');
-    var ele = $(`.user-label[href='${href}'] + .user-hover-card-anchor`).$('.user-info-card-header');
+    var ele = $(`.user-label[href='${href}'] + .user-hover-card-anchor`).find('.user-info-card-header');
     var html = ele.innerHTML;
     if (!html.includes('</button>')) { // doesnt place multiple buttons
         ele.innerHTML = html + '<button class="dmBtn">Message</button>';
