14b1fc90e46b9d0e8984769a0edc8cf0b4abc98d
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 19:03:46 +0000

    this is nice

--- foreground.js
+++ foreground.js
@@ -1,15 +1,12 @@
 console.log('foreground baby');
 
 $('html').on('DOMSubtreeModified', 'body', () => {
+    console.log('html changed nice :>')
     $('.user-hover-card .user-info-card-header span.jsx-1369737386:not(.user-info-card-full-name)').after().click(() => {
         var username = $('.user-hover-card-anchor span.jsx-801033477').text().split(" ")[0];
         console.log(username);
-    })
+    }) 
 });
-
-window.addEventListener("hashchange", () => {
-    console.log('page changed');
-}, false);
 
 var html = `
 <div class='repldmBtn'>
