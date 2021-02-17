d5cb8b8fe874d899ccbdfeaa62729cf0b0e86f9b
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-17 13:56:35 +0000

    ffs im so goddamn stupid

--- foreground.js
+++ foreground.js
@@ -3,8 +3,8 @@
 $('.user-label').hover(() => {
     var href = $(this).attr('href');
     var ele = $(`.user-label[href='${href}'] + .user-hover-card-anchor`).find('.user-info-card-header');
-    var html = ele.innerHTML;
+    var html = ele.html();
     if (!html.includes('</button>')) { // doesnt place multiple buttons
-        ele.innerHTML = html + '<button class="dmBtn">Message</button>';
+        ele.html(html + '<button class="dmBtn">Message</button>');
     };
 })
