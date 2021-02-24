2a1bad2bd698aea6bd4b29d17f5b34dd5332b772
Marcus Weinberger <marcusjw.ftw@gmail.com>
2021-02-24 14:06:09 +0000

    fix read

--- foreground.js
+++ foreground.js
@@ -132,11 +132,10 @@ function displaySentMessage(message) {
 function checkReadStatus() {
     var ids = [];
     Array.from(document.getElementsByClassName('msg-node')).forEach((el) => {
-        if (isElementVisible(el)) {
-            var msg = JSON.parse(el.getElementsByClassName('hidden-input')[0].value);
-            if (!(msg.read)) {
-                ids.push(msg.id);
-            }
+        //TODO: check if el
+        var msg = JSON.parse(el.getElementsByClassName('hidden-input')[0].value);
+        if (!(msg.read)) {
+            ids.push(msg.id);
         }
     })
     markRead(ids, (res) => {
@@ -211,27 +210,6 @@ function loadConvo(user) {
         })
     }
     setTimeout(checkReadStatus, 1000);
-}
-
-// https://stackoverflow.com/a/15203639/8291579
-function isElementVisible(el) {
-    var rect     = el.getBoundingClientRect(),
-        vWidth   = window.innerWidth || document.documentElement.clientWidth,
-        vHeight  = window.innerHeight || document.documentElement.clientHeight,
-        efp      = function (x, y) { return document.elementFromPoint(x, y) };     
-
-    // Return false if it's not in the viewport
-    if (rect.right < 0 || rect.bottom < 0 
-            || rect.left > vWidth || rect.top > vHeight)
-        return false;
-
-    // Return true if any of its four corners are visible
-    return (
-          el.contains(efp(rect.left,  rect.top))
-      ||  el.contains(efp(rect.right, rect.top))
-      ||  el.contains(efp(rect.right, rect.bottom))
-      ||  el.contains(efp(rect.left,  rect.bottom))
-    );
 }
 
 function authed() {
