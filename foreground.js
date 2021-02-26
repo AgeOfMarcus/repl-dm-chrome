f2cfbd64fbce2493d9efc006a784687d7319989e
Rafi Asmoucha <rasmoucha@gmail.com>
2021-02-26 11:15:08 +0000

    a

--- foreground.js
+++ foreground.js
@@ -137,10 +137,10 @@ function init() {
                             <div class='mid' status='${((recent.from == user) ? 'received' : 'sent')}'> 
                                 <div class='name'>${user}</div>
                                 <div class='icons' date='${time_ago(new Date(recent.time))}'>
-                                    <i class="far fa-paper-plane sent-icon"></i>
-                                    <i class="fas fa-paper-plane opened-icon"></i>
+                                    <i class="far fa-paper-plane read-icon"></i>
+                                    <i class="fas fa-paper-plane sent-icon"></i>
                                     <i class="far fa-comment-alt received-icon"></i>
-                                    <i class="fas fa-comment-alt read-icon"></i>
+                                    <i class="fas fa-comment-alt opened-icon"></i>
                                 </div>
                             </div>
                         </label>
